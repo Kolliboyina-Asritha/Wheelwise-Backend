b5b69d02d65b29694e1ac2e9d73b40b08a588282
Kolliboyina-Asritha <asrithaasritha99@gmail.com>
2025-07-25 19:54:34 +0530

    Update credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,11 +1,15 @@
 const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  next();
+    const origin = req.headers.origin;
+    console.log(`🟡 Credentials Middleware → Origin: ${origin}`);
+    if (allowedOrigins.includes(origin)) {
+        console.log(`🟢 Origin Allowed: ${origin}`);
+        res.header('Access-Control-Allow-Credentials', true);
+    } else {
+        console.log(`🔴 Origin Blocked: ${origin}`);
+    }
+    next();
 };
 
 module.exports = credentials;
